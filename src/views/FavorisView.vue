08d8fa0f2ea3c3e50c64ff077f51913d8728e109
Guewen Carre <guewencarre@gmail.com>
2023-04-20 19:22:10 +0200

    ✨  add  FavorisView

---
+++ src/views/FavorisView.vue
@@ -0,0 +1,29 @@
+<template>
+    <FavorisComponent></FavorisComponent>
+
+
+</template>
+
+
+
+<script>
+import FavorisComponent from '@/components/FavorisComponent.vue';
+
+export default {
+    name: 'FavorisView',
+    components:{
+        FavorisComponent
+    }
+}
+
+
+</script>
+
+
+
+
+<style scoped>
+
+
+
+</style>
