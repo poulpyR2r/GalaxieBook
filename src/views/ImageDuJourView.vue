744aa4b0cf1cc40742a5db0c4eb508e4c60dcafe
Guewen Carre <guewencarre@gmail.com>
2023-04-20 19:23:49 +0200

    ✨ create ImageView

---
+++ src/views/ImageDuJourView.vue
@@ -0,0 +1,29 @@
+<template>
+    <ImageDuJourComponent></ImageDuJourComponent>
+
+
+</template>
+
+
+
+<script>
+import ImageDuJourComponent from '@/components/ImageDuJourComponent.vue';
+
+export default {
+    name: 'ImageDuJourView',
+    components:{
+        ImageDuJourComponent
+    }
+}
+
+
+</script>
+
+
+
+
+<style scoped>
+
+
+
+</style>
